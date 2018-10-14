ccb784fee51cb95744013091904cb3a695f625a1
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2018-10-14 00:51:06 -0700

    ggplot part 1, ch 2 - data

---
+++ 02_ggplot_data.R
@@ -0,0 +1,22 @@
+# 
+# 
+# author: Alexander C. Hungerford
+# 
+# created: 13 October 2018
+#
+# course: Data Visualization with ggplot2 - Data
+
+# load libraries & packages
+library(tidyverse)
+
+# load data
+data("mtcars")
+data("iris")
+load("gg_diamonds.RData")
+# load("gg_fish.RData")
+# load("gg_recess.RData")
+
+
+# There is an advantage to plotting out a graph with just the df and aes(), bc
+# you can always add the other layers later
+
