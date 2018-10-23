b21aa27c935704bcad46c5660157f28dde7359f3
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2018-10-23 01:10:14 -0700

    renamed Rscript, created loops script

---
+++ 02_intermediateR_loops.R
@@ -0,0 +1,42 @@
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# Title: Loops
+
+# while loop
+# 
+# set up your counter outside the loop
+#
+# while (condition){
+#   expr
+#   increment or decrement counter
+# }
+
+# example of while loop
+ctr <- 1
+
+while(ctr <= 5){
+  print(paste("ctr is set to", ctr))
+  ctr <- ctr + 1
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
