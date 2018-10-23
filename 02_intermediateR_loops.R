7531bcb3a94f7171aa461fcd692671bfa9cc9cc1
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2018-10-23 01:21:11 -0700

    updated while loop with a break statement

--- 02_intermediateR_loops.R
+++ 02_intermediateR_loops.R
@@ -6,26 +6,37 @@
 # 
 # 
 # Title: Loops
+#
+# Summary:
+#   crtl-C terminates a while loop
+#
+#   while loops & break statements
+
+
 
 # while loop
-# 
+
 # set up your counter outside the loop
-#
 # while (condition){
 #   expr
 #   increment or decrement counter
 # }
 
-# example of while loop
-ctr <- 1
 
+# example
+ctr <- 1
 while(ctr <= 5){
   print(paste("ctr is set to", ctr))
   ctr <- ctr + 1
 }
 
-
-
+# break statement example: terminate loop as soon as ctr is divisible by 5.
+ctr <- 0
+while(ctr <= 7){
+  if(ctr %% 5 == 0)
+    break
+}
+print(paste("ctr is set to", ctr))
 
 
 
