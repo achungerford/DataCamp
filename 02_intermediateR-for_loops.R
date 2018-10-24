d4f0f990fa90674cfcd5fe153a969eae1f2f684f
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2018-10-24 13:28:34 -0700

    next statement

--- 02_intermediateR-for_loops.R
+++ 02_intermediateR-for_loops.R
@@ -54,6 +54,27 @@ for(city in cities){
 
 
 
+# next statement in a for loop
+cities <- list("New York", "Paris",
+               "London", "Tokyo",
+               "Rio de Janiero", "Cape Town")
+
+# a next statement skips remainder of the code to next iteration.
+# London is not printed
+for(city in cities){
+  if( nchar(city) == 6){
+    next
+  }
+  print(city)
+}
+
+
+
+
+
+# another way to loop over data: for loop v2.0
+
+
 
 
 
