45e14b35b27a072c8c7f3d97a639d609651c579f
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2018-10-24 15:46:58 -0700

    tic-tac-toe matrix created

--- 02_intermediateR-for_loops.R
+++ 02_intermediateR-for_loops.R
@@ -1,9 +1,9 @@
 # 
+# author: Alexaner C. Hungerford
 # 
+# Intermediate R: for loops
 # 
-# 
-# 
-# 
+# Date: 24 October 2018
 # 
 # 
 # 
@@ -106,6 +106,28 @@ for(i in nyc){
 # same as above, but notice the need for the double brackets
 for(i in 1:length(nyc)){
   print(nyc[[i]])
+}
+
+
+
+# create a blank tic-tac-toe matrix
+ttt <- matrix( , 3, 3)
+ttt
+
+# fill in specific values
+ttt[1, 1] = "O"
+ttt[1, 3] = "X"
+ttt[2, 2] = "O"
+ttt[2, 3] = "O"
+ttt[3, 1] = "X"
+ttt[3, 3] = "X"
+
+ttt
+
+for(i in 1:nrow(ttt)){
+  for(j in 1:ncol(ttt)){
+    print(paste("On row ", i, " and column ", j ," the board contains ", ttt[i,j]))
+  }
 }
 
 
