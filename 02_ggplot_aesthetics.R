8dc02fe5852acb416bbee65e9c750874b922d818
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2018-10-30 16:11:12 -0700

    change size/shape of points

--- 02_ggplot_aesthetics.R
+++ 02_ggplot_aesthetics.R
@@ -12,8 +12,30 @@
 # 
 ############################################### 
 
+# Note, plots will look different than the Datacamp course in color gradients.
+# It's not clear why, probably because of the version of ggplot that was used.
+
+
 library(tidyverse)
 
+# load and view the data
+data(mtcars)
+View(mtcars)
 
 
+# 1 - Map mpg to x and cyl to y
+ggplot(mtcars, aes(x = mpg,y = cyl)) + geom_point()
 
+# 2 - Reverse: Map cyl to x and mpg to y
+ggplot(mtcars, aes(x = cyl, y = mpg)) + geom_point()
+
+# 3 - Map wt to x, mpg to y and cyl to col
+ggplot(mtcars, aes(x = wt, y = mpg, col = cyl)) + geom_point()
+
+# 4 - Change shape and size of the points in the above plot
+ggplot(mtcars, aes(x = wt, y = mpg, col = cyl)) +
+  geom_point(shape = 1, size = 4)
+
+
+####################### NOTES #################################################
+#
