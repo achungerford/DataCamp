27175883a6d857fdf7781043ba40fe3043f74a09
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2018-10-30 16:23:58 -0700

    changed size, shape, fill, transparency on plot

--- 02_ggplot_aesthetics.R
+++ 02_ggplot_aesthetics.R
@@ -36,6 +36,44 @@ ggplot(mtcars, aes(x = wt, y = mpg, col = cyl)) + geom_point()
 ggplot(mtcars, aes(x = wt, y = mpg, col = cyl)) +
   geom_point(shape = 1, size = 4)
 
+# 'am' and 'cyl' are factors; 'wt' is numeric
+# next, map cyl to fill, change shape, and alpha
+ggplot(mtcars, aes(x = wt, y = mpg, fill = cyl)) +
+  geom_point(shape = 21, size = 4, alpha = 0.6)
+
 
 ####################### NOTES #################################################
+# The color aesthetic typically changes the outside outline of an object 
+# and the fill aesthetic is typically the inside shading. However, 
+# as you saw in the last exercise, geom_point() is an exception. 
+# 
+# Here you use color, instead of fill for the inside of the point. 
+# But it's a bit subtler than that.
+# 
+# Which shape to use? 
+# The default geom_point() uses shape = 19 (a solid circle with an outline the same 
+# colour as the inside). 
+# Good alternatives are shape = 1 (hollow) and shape = 16 (solid, no outline). 
+# These all use the col aesthetic (don't forget to set alpha for solid points).
 #
+# A really nice alternative is shape = 21
+# which allows you to use both fill for the inside and col for the outline!
+# This is a great little trick for when you want to map two aesthetics to a dot.
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
