07ac1b9e12979c49e75c6d988687931417c5dec8
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2018-11-12 12:47:37 -0800

    moved files

--- gg/02_ggplot_aesthetics.R
+++ gg/02_ggplot_aesthetics.R
@@ -132,6 +132,29 @@ ggplot(mtcars, aes(x = wt, y = mpg, fill = cyl)) +
 
 # Exercise: Going All Out
 
+# Map mpg onto x, qsec onto y and factor(cyl) onto col
+ggplot(mtcars, aes(x = mpg, y = qsec, col = factor(cyl))) +
+  geom_point()
+
+
+# Add mapping: factor(am) onto shape
+ggplot(mtcars, aes(x = mpg,
+                   y = qsec, 
+                   col = factor(cyl),
+                   shape = factor(am))) + 
+  geom_point()
+
+
+# Add mapping: (hp/wt) onto size
+ggplot(mtcars, aes(x = mpg,
+                   y = qsec,
+                   col = factor(cyl),
+                   shape = factor(am),
+                   size = (hp/wt))) +
+  geom_point()
+
+###############################################################################
+
 
 
 ####################### NOTES #################################################
