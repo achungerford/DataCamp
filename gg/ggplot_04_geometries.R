9f46b95488840fc23a24a3e0f5386caf1cd98278
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2018-11-19 07:05:45 -0800

    ggplot, ch 4, geometries

---
+++ gg/ggplot_04_geometries.R
@@ -0,0 +1,11 @@
+#
+#
+# author: Alexander C. Hungerford
+# 
+# date created: 30 October 2018
+# 
+# course: DataCamp
+#         Data Visualization with ggplot2
+#         Chapter 4: Geometries
+# 
+############################################################################### 
