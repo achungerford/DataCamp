d5ec2e499a52701f12992a88ddb5bb38bd4180fc
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2018-11-21 09:40:17 -0800

    function accepts 1 value in a vector

--- intermediateR/03_functions-writing_functions.R
+++ intermediateR/03_functions-writing_functions.R
@@ -139,6 +139,23 @@ b <- increment(count)
 count <- increment(count, 2)
 
 
+# Exercise: R you functional
+# writing a function that can interpret a single value of the vector
+linkedin <- c(16, 9, 13, 5, 2, 17, 14)
+facebook <- c(17, 7, 5, 16, 8, 13, 14)
 
+interpret <- function(num_views){
+  if(num_views > 15){
+    print("You're popular!")
+    return(num_views)
+  } else{
+    print("Try to be more visible!")
+    return(0)
+  }
+}
 
+interpret(linkedin[1])
+interpret(facebook[2])
+# what is "annoying" about the function 'interpret'? It only takes one argument
 
+# now, let's write a function that interprets the entire vector
