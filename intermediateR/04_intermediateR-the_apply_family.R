9a926128e1c7891b83f40baf07bb2d1ded1b98c2
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2018-11-24 22:52:53 -0800

    lapply()

--- intermediateR/04_intermediateR-the_apply_family.R
+++ intermediateR/04_intermediateR-the_apply_family.R
@@ -9,3 +9,17 @@
 
 library(tidyverse)
 
+# when you get vectors or lists - for loops, or lapply
+
+nyc <- list(pop = 8405837,
+            borgouhs = c("Manhattan", "Bronx", "Brooklyn",
+                         "Queens", "Staten Island"),
+            capital = FALSE)
+
+# say you want the class of each element in the list - for loop or lapply
+for(info in nyc){
+  print(class(info))
+}
+
+# less code
+lapply(nyc, class)
