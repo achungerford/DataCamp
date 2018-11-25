e4c165984054cae20f12008b2c9016b2feb366b6
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2018-11-24 23:37:16 -0800

    lapply() on fnctn with multiple args

--- intermediateR/04_intermediateR-the_apply_family.R
+++ intermediateR/04_intermediateR-the_apply_family.R
@@ -44,12 +44,27 @@ lapply(cities, nchar)
 # if you want to convert the list to a vector, just wrap it in an unlist()
 unlist(lapply(cities, nchar))
 
-# you can use lapply() with functions you wrote yourself
+# you can use lapply() with functions you wrote yourself - create function
 oil_prices <- list(2.37, 2.49, 2.18, 2.22, 2.47, 2.32)
 triple <- function(x){
   3 * x
 }
+# get result
 result <- lapply(oil_prices, triple)
 str(result)
 # make output a vector instead of list
 unlist(result)
+
+# try to make 'triple' more geneirc, call it multiply, and have it use 2 arguments
+multiply <- function(x, factor){
+  x * factor
+}
+# now we can choose which factor we want to multiply by
+
+# what if you want to use multiple inside lapply()?
+# specify other arguments after
+times3 <- lapply(oil_prices, multiply, factor = 3)
+unlist(times3)
+
+times4 <- lapply(oil_prices, multiply, factor = 4)
+unlist(times4)
