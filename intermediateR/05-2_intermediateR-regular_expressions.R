d893f8b106729c462cacb03293b2434f4f7693f9
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2018-11-27 14:39:45 -0800

    date, time exercises

--- intermediateR/05-2_intermediateR-regular_expressions.R
+++ intermediateR/05-2_intermediateR-regular_expressions.R
@@ -79,3 +79,121 @@ gsub(pattern = "a|i", replacement = "_", x = animals)
 
 # using vertical bar more 
 gsub(pattern = "a|i|o", replacement = "_", x = animals)
+
+
+###################### Exercise: grepl() & grep() #############################
+
+# The emails vector has already been defined for you
+emails <- c("john.doe@ivyleague.edu", "education@world.gov",
+            "dalai.lama@peace.org", "invalid.edu",
+            "quant@bigdatacollege.edu", "cookie.monster@sesame.tv")
+
+# Use grepl() to match for "edu"
+grepl(pattern = "edu", emails)
+
+# Use grep() to match for "edu", save result to hits
+# Do the same thing with grep(), but this time save the resulting indexes
+# in a variable hits
+hits <- grep(pattern = "edu", x= emails)
+
+# Subset emails using hits
+# Use the variable hits to select from the emails vector
+# only the emails that contain "edu"
+emails[hits]
+
+
+################## Exercise: grepl() & grep() (2) #############################
+
+# The emails vector has already been defined for you
+emails <- c("john.doe@ivyleague.edu", "education@world.gov",
+            "dalai.lama@peace.org", "invalid.edu",
+            "quant@bigdatacollege.edu", "cookie.monster@sesame.tv")
+
+# Use grepl() to match for .edu addresses more robustly
+# Use grepl() with the more advanced regular expression to
+# return a logical vector. Simply print the result.
+grepl(pattern = "@.*\\.edu$", x = emails)
+# ".*"  which matches any character (.) zero or more times (*).
+# Both the dot and the asterisk are metacharacters.
+# You can use them to match any character between
+# the @-sign and the ".edu" portion of an email address.
+
+
+# Use grep() to match for .edu addresses more robustly, save result to hits
+# Do a similar thing with grep() to create a vector of indices.
+# Store the result in the variable hits
+hits <- grep(pattern = "@.*\\.edu$", emails)
+
+# Subset emails using hits
+emails[hits]
+
+# A careful construction of our regular expression leads to more
+# meaningful matches. However, even our robust email selector will often match
+# some incorrect email addresses, for instance, 
+# kiara@@fakemail.edu
+# Let's not worry about this too much and continue 
+
+
+############### Exercise: sub() & gsub() ######################################
+
+# for specifying "replacement" arguments
+
+emails
+
+sub(pattern = "@.*\\.edu$", replacement = "@datacamp.edu", x = emails)
+
+
+awards <- c("Won 1 Oscar.",
+            "Won 1 Oscar. Another 9 wins & 24 nominations.",
+            "1 win and 2 nominations.",
+            "2 wins & 3 nominations.",
+            "Nominated for 2 Golden Globes. 1 more win & 2 nominations.",
+            "4 wins & 1 nomination.")
+
+sub(".*\\s([0-9]+)\\snomination.*$", "\\1", awards)
+
+
+#   .*
+#   It can be read as "any character that is matched zero or more times".
+
+#   \\s: Match a space. 
+#       The "s" is normally a character, escaping it (\\) makes it a metacharacter.
+
+#   [0-9]+: Match the numbers 0 to 9, at least once (+).
+
+#   ([0-9]+): The parentheses are used to make parts of the matching string available
+#             to define the replacement.
+#
+#             ([0-9]+) selects the entire number that comes before the word
+#             “nomination” in the string
+#
+#   \\1 in the replacement argument of sub() is set to the string
+#       captured by the regular expression [0-9]+
+
+
+
+
+
+############ NOTES ############################################################
+#
+# You can use the caret, ^, and the dollar sign, $ to match the content located
+# in the start and end of a string, respectively.
+#
+# This could take us one step closer to a correct pattern for matching
+# only the ".edu" email addresses from our list of emails.
+# But there's more that can be added to make the pattern more robust:
+#
+# @, because a valid email must contain an at-sign.
+#
+# ".*"  which matches any character (.) zero or more times (*).
+# Both the dot and the asterisk are metacharacters.
+# You can use them to match any character between
+# the @-sign and the ".edu" portion of an email address.
+#
+# \\.edu$
+#     to match the ".edu" part of the email at the end of the string.
+#
+#     The \\ escapes the dot: it tells R that you want to use the "."
+#     as an actual character.
+#
+
---
+++ intermediateR/05-3_intermediateR-dates-times.R
@@ -0,0 +1,104 @@
+#
+# author: Alexander C. Hungerford
+#
+# created: 27 November 2018
+# 
+# title: DataCamp
+#        Intermediate R
+#        Ch. 5 - Utilities - times & dates
+
+library(tidyverse)
+
+today <- Sys.Date()
+
+today
+class(today)
+
+now <- Sys.time()
+now
+class(now)
+
+# create Date objects: say we want a date object for 14 May 1971
+my_date <- as.Date("1971-05-14")
+my_date
+
+class(my_date)
+
+# say we scramble the order of the date elments -- error
+my_date <- asDate("1971-14-05")
+
+# Default format: %Y-%m-%d
+# %Y 4-digit year
+# %m 2-digit month
+# %d 2-digit day
+
+# but we can fix it if we alter these explicitly
+my_date <- as.Date("1971-14-05", format = "%Y-%d-%m")
+my_date
+
+
+# creating POSIXct objects
+my_time <- as.POSIXct("1971-05-14 11:25:15")
+my_time
+
+
+
+# calculations with dates
+
+# increment by 1 day
+my_date
+my_date + 1
+
+# get difference between: 29 Sept. 1998 and my_date
+my_date2 <- as.Date("1998-09-29")
+
+my_date2 - my_date
+
+
+# calculations wth POSIXct objects
+# time unit is 1 second
+my_time + 1
+
+my_time2 <- as.POSIXct("197-07-14 21:11:55")
+
+my_time - my_time2
+
+
+#################### Exercise: Create & format dates ##########################
+
+# Definition of character strings representing dates
+str1 <- "May 23, '96"
+str2 <- "2012-03-15"
+str3 <- "30/January/2006"
+
+# Convert the strings to dates: date1, date2, date3
+date1 <- as.Date(str1, format = "%b %d, '%y")
+date2 <- as.Date(str2, format = "%Y-%m-%d")
+date3 <- as.Date(str3, format = "%d/%B/%Y")
+
+
+# Convert dates to formatted strings
+format(date1, "%A")
+format(date2, "%d")
+format(date3, "%b %Y")
+
+
+################## Exercise: Create & format times ############################
+
+# Definition of character strings representing times
+str1 <- "May 23, '96 hours:23 minutes:01 seconds:45"
+str2 <- "2012-3-12 14:23:08"
+
+# Convert the strings to POSIXct objects: time1, time2
+time1 <- as.POSIXct(str1, format = "%B %d, '%y hours:%H minutes:%M seconds:%S")
+time2 <- as.POSIXct(str2)
+
+# Convert times to formatted strings
+# Using format(), create a string from time1 containing only the minutes.
+format(time1, "%M")
+
+# From time2, extract the hours and minutes as "hours:minutes AM/PM".
+# Refer to the assignment text above to find the correct conversion symbols!
+format(time2, "%I:%M %p")
+
+
