f38fdeb805222222913e8eb6cd1dfd74ee2893a5
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2018-12-11 00:40:26 -0800

    read in data

--- data_import_1/01-01_importing.R
+++ data_import_1/01-01_importing.R
@@ -13,4 +13,37 @@
 
 
 
+# read a csv - if the file is in your current working directory
+# read.csv("filename.csv", stringsAsFactors = FALSE)
+
+
+# if the file is not in your current working directory,
+  # you can build a path to a file in a 'file-independent' way with file.path()
+  # say it's in the "datasets" folder of the home directory
+# path <- file.path("~", "datasets", "filename.csv")
+      # now: read.csv(path, stringsAsFactors = FALSE)
+
+
+# import data
+pools <- read.csv("data_import_1/swimming_pools.csv", stringsAsFactors = FALSE)
+
+# look at structure
+str(pools)
+
+# look at first few
+head(pools)
+
+
+# look at which variables change type if stringsAsFactors = TRUE,
+    # the strings change from strings to factors
+        # which would be for categorical variables
+p <- read.csv("data_import_1/swimming_pools.csv", stringsAsFactors = TRUE)
+str(p)
+
+# rm(p)
+
+
+# reading delimiters ------------------------------------------------------
+
+
 
